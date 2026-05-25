c496856c09e683b4ccb9852391356f94051b2a6f
NKbharath <bharathnk2004@gmail.com>
2026-05-25 14:33:35 +0530

    verification-issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,7 @@ function Home() {
   return (
     <div className="">
       <Helmet>
-        <meta
-          name="google-site-verification"
-          content="9kB8OLYKpRk2txUjtd_5ReLVrMvC6n4u47Ts9hQSZFE"
-        />
+        
         <title>
           Best Overseas Education Consultancy in Namakkal | Seyon Overseas
         </title>
